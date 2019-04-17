f2c0e6fc950d84cd5a88d9678b4a578ed76c1def
krishnamurthyav <drkrishna222@gmail.com>
2019-04-17 19:16:12 +0530

    squareRoot function using typescript

---
+++ Algorithm/sqrt.ts
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *  @file           : insertionSort.ts
+ *  @overview       : Sort using Inseretion-Sort method
+ *  @author         : Krishna Murthy A V 
+ *  @version        : 1.0
+ *  @since          : 15-April-2019
+ *
+ ******************************************************************************/
+// import readline-sync function
+import readline = require('readline-sync');
+
+//function definition
+function sqrt(){
+    var number:number = readline.question("Enter the number to find its Square Root");
+    squareRoot(number);
+
+    function squareRoot(num){
+        if(num > 0){
+            var value:number = num;
+            var epsilon = 1e-15;
+            while(Math.abs(value-num/value)>epsilon*value){
+                value=(num/value+value)/2;
+            }
+            console.log("square root of non negative number is : "+value);
+        }
+        else{
+            console.log("Invalid or negative number");
+        }
+    }
+}
+
+//function call
+sqrt();
